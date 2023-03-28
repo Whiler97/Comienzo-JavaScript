98992d8b9ba5b5cf2d83b3dcba6810cf46c666cf
Lautaro Campos <Whiler.977@gmail.com>
2023-03-28 20:55:13 -0300

    agregando cosas

---
+++ login.js
@@ -0,0 +1,15 @@
+
+function loguear()
+{
+    let user=document.getElementById("usuario").value;
+    let pass=document.getElementById("contraseña").value;
+}
+
+if(user=="Juan" && pass=="1234")
+{
+    window.location="bienvenida.html";
+}
+
+else{
+alert("Datos incorrectos");
+}
